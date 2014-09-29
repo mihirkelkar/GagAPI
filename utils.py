100de73d44adf09c72431f92d5764bfeb7af46e4
mihirkelkar <kelkarmhr@gmail.com>
2014-09-29 19:40:59 -0400

    Adding the filters for the API

--- utils.py
+++ utils.py
@@ -22,17 +22,17 @@ def retrieve_articles(number_of_pages = 1):
     if content == None:
       return None
     extend_url = content.find('a', attrs={'class' : 'btn badge-load-more-post'})['href']
-    print extend_url
+    #print extend_url
     all_articles += content.findAll("article")
     number_of_pages -= 1
-    print all_articles[0]
+    #print all_articles[0]
   return all_articles
 
 #Add filters such that the user can go ahead and limit whether
 #they want only gifs, images, posts with comments above this number,#posts with comments greater than 
 #Make sure that the page number limit is 100. 
  
-def annotate(number_of_pages = 1, filter = None):
+def annotate(number_of_pages):
   final_result = list()
   for ii in retrieve_articles(number_of_pages):
     #TODO : Make the dictionary by getting other elements out. 
@@ -46,21 +46,34 @@ def annotate(number_of_pages = 1, filter = None):
     votes = ii['data-entry-votes']
     comments = ii['data-entry-comments']
     title = ii.find('img', attrs={'class':'badge-item-img'})['alt'] 
-    print title
+    #print title
     final_result.append({
       "type" : type , 
       "post_url" : post_url,
-      "votes" : votes,
-      "comments" : comments,
+      "votes" : int(votes),
+      "comments" : int(comments),
       "title" : title,
       "media_url" : media_url
     })
   return final_result
 
+def get_posts_from_page(number_of_pages = 1, media_type = 'all', page_type = None, more_votes_than = 0, more_comments_than = 0):
+  data = annotate(number_of_pages)
+  if media_type == 'gif':
+    data = [el for el in data if el['type'] == 'GIF']
+  elif media_type == 'image':
+    data = [el for el in data if el['type'] == 'Image']
+  else:
+    pass 
+  if more_votes_than > 0:
+    data = [el for el in data if el['votes'] > more_votes_than]
+  if more_comments_than > 0:
+    data = [el for el in data if el['comments'] > more_comments_than]
+  return data
+
 def main():
-  for ii in annotate():
+  for ii in get_posts_from_page(number_of_pages = 4, more_votes_than = 10000, more_comments_than  = 200):
     print ii
-    print "============"
-  
+    print "========="  
 if __name__ == "__main__":
   main()
